b836726e2d0ae87eb05d025802f61e2025798f29
Prince Advance <princeadvanceagbenu@gmail.com>
2023-05-14 16:29:35 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import tkinter as tk
+import PyPDF2
+from PIL import Image, ImageTk
+from tkinter.filedialog import askopenfile
+
+root = tk.Tk() # this represents the interface of the code
+
+canvas = tk.Canvas(root, width = 600, height = 300)
+canvas.grid(columnspan = 3, rowspan = 3)
+
+# logo
+logo = Image.open('logo.png') # opening the image and taking the URL of the logo
+logo = ImageTk.PhotoImage(logo) # Converting the logo into a tkinter image
+logo_label = tk.Label(image = logo)
+logo_label.image = logo
+logo_label.grid(column = 1, row = 0)
+
+# Title
+root.title("pdf extractor")
+root.geometry("600x400")
+
+# instructions
+instructions = tk.Label(root, text ="Select a PDF file on your computer to extract all its texts", font = "Raleway")
+instructions.grid(columnspan = 3, column = 0, row = 1)
+
+def open_file():
+    browse_text.set("loading...")
+    file = askopenfile(parent=root, mode='rb', title='choose a file',filetype=[('Pdf file', '*.pdf')])
+    if file:
+        read_pdf = PyPDF2.PdfReader(file)
+        page = read_pdf.pages[0]
+        page_content = page.extract_text()
+        print(page_content)
+        
+        # text box
+        text_box = tk.Text(root, height = 10, width = 50, padx = 15, pady = 15)
+        text_box.insert(1.0, page_content)
+        text_box.grid(column = 1,row = 3)
+
+
+#browse button
+browse_text = tk.StringVar()
+browse_btn = tk.Button(root, textvariable = browse_text, command = lambda:open_file(), font = "Raleway", bg = '#20bebe',fg = "white", height = 2, width = 15)
+browse_text.set("Browse")
+browse_btn.grid(column = 1, row = 2)
+
+canvas = tk.Canvas(root, width = 600, height = 250)
+canvas.grid(columnspan = 3)
+
+root.mainloop()
+
+# the 2 lines of code above represents everything in the window so every code outside of it will not function
